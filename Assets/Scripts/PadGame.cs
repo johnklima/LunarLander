604a8382b5ff868366a75e01d466c043d0969890
johnklima <klima.de.sa@gmail.com>
2023-11-07 18:49:19 +0100

    clean me

--- Assets/Scripts/PadGame.cs
+++ Assets/Scripts/PadGame.cs
@@ -7,17 +7,7 @@ public class PadGame : MonoBehaviour
 
     private int padsLanded = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
     public void addPad()
     {
